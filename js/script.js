aae6b8e60e32ddce1056d688a3f5451311d48d0d
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-27 01:09:57 -0300

    Declaring variables

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const question = document.querySelector('#question');
+const answersBox = document.querySelector('#answers-box');
+const quizContainer = document.querySelector('#quiz-container');
+const scoreContainer = document.querySelector('#score-container');
+const letters = ['a', 'b', 'c', 'd'];
+let points = 0;
+let actualQuestion = 0;
