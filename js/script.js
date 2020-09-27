c9f1336f38a3208636dab4e4329fbb42ffe2d27e
Lucas Andrade <lafdesouza2002@outlook.com>
2020-09-27 01:27:51 -0300

    Creating initial functions

--- js/script.js
+++ js/script.js
@@ -7,67 +7,85 @@ let points = 0;
 let actualQuestion = 0;
 
 const questions = [
-    {
-      "question": "PHP was developed for?",
-      "answers": [
-        {
-          "answer": "back-end",
-          "correct": true
-        },
-        {
-          "answer": "front-end",
-          "correct": false
-        },
-        {
-          "answer": "Sistema operacional",
-          "correct": false
-        },
-        {
-          "answer": "Banco de dados",
-          "correct": false
-        },
-      ]
-    },
-    {
-      "question": "One way of variable declaration in Javascript",
-      "answers": [
-        {
-          "answer": "$var",
-          "correct": false
-        },
-        {
-          "answer": "var",
-          "correct": true
-        },
-        {
-          "answer": "@var",
-          "correct": false
-        },
-        {
-          "answer": "#let",
-          "correct": false
-        },
-      ]
-    },
-    {
-      "question": "What's the selector id in CSS",
-      "answers": [
-        {
-          "answer": "#",
-          "correct": true
-        },
-        {
-          "answer": ".",
-          "correct": false
-        },
-        {
-          "answer": "@",
-          "correct": false
-        },
-        {
-          "answer": "/",
-          "correct": false
-        },
-      ]
-    },
-  ]
+  {
+    question: 'PHP was developed for?',
+    answers: [
+      {
+        answer: 'back-end',
+        correct: true,
+      },
+      {
+        answer: 'front-end',
+        correct: false,
+      },
+      {
+        answer: 'Sistema operacional',
+        correct: false,
+      },
+      {
+        answer: 'Banco de dados',
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: 'One way of variable declaration in Javascript',
+    answers: [
+      {
+        answer: '$var',
+        correct: false,
+      },
+      {
+        answer: 'var',
+        correct: true,
+      },
+      {
+        answer: '@var',
+        correct: false,
+      },
+      {
+        answer: '#let',
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "What's the selector id in CSS",
+    answers: [
+      {
+        answer: '#',
+        correct: true,
+      },
+      {
+        answer: '.',
+        correct: false,
+      },
+      {
+        answer: '@',
+        correct: false,
+      },
+      {
+        answer: '/',
+        correct: false,
+      },
+    ],
+  },
+];
+
+function init() {
+  createQuestion(0);
+}
+
+init();
+
+function createQuestion(i) {
+  const oldButton = answersBox.querySelectorAll('button');
+  oldButton.forEach((btn) => {
+    btn.remove();
+  });
+  const questionText = question.querySelector('#question-text');
+  const questionNumber = question.querySelector('#question-number');
+
+  questionText.textContent = questions[i].question;
+  questionNumber.textContent = i + 1;
+}
